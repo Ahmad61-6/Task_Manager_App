46b9c5baa5af24745cedbd3e28b7f9baf51a030c
Ahmad Bin Mijanur Rahman <130034466+Ahmad61-6@users.noreply.github.com>
2024-05-12 02:08:14 +0600

    Update pin_verification.dart

--- lib/ui/screens/pin_verification.dart
+++ lib/ui/screens/pin_verification.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:task_manager_app/data.network_caller/network_caller.dart';
+import 'package:task_manager_app/data.network_caller/utility/urls.dart';
 import 'package:task_manager_app/style.dart';
 import 'package:task_manager_app/ui/screens/sign_in_screen.dart';
-
 import 'package:task_manager_app/ui/widgets/body_background.dart';
-import 'package:pin_code_fields/pin_code_fields.dart';
+
+import '../contollers/password_verification_sharedpreference.dart';
 import 'reset_password.dart';
 
 class PinVerificationScreen extends StatefulWidget {
@@ -15,6 +17,10 @@ class PinVerificationScreen extends StatefulWidget {
 }
 
 class _PinVerificationScreenState extends State<PinVerificationScreen> {
+  final TextEditingController _otpVTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool _pinVerificationInProgress = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,105 +29,150 @@ class _PinVerificationScreenState extends State<PinVerificationScreen> {
           child: Padding(
             padding: const EdgeInsets.all(24.0),
             child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  const Text(
-                    "PIN Verification",
-                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(
-                    height: 18,
-                  ),
-                  const Text(
-                    "A 6 digit verification pin will send to your\nemail address",
-                    style: TextStyle(fontSize: 16, color: Colors.grey),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  PinCodeTextField(
-                    appContext: context,
-                    length: 6,
-                    obscureText: false,
-                    animationType: AnimationType.fade,
-                    pinTheme: PinTheme(
-                      shape: PinCodeFieldShape.box,
-                      borderRadius: BorderRadius.circular(5),
-                      fieldHeight: 50,
-                      fieldWidth: 40,
-                      activeFillColor: Colors.white,
-                      selectedFillColor: Colors.white,
-                      inactiveFillColor: Colors.white,
-                      activeColor: Colors.greenAccent,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 80,
                     ),
-                    animationDuration: const Duration(milliseconds: 300),
-                    enableActiveFill: true,
-                    onCompleted: (v) {},
-                    onChanged: (value) {},
-                    beforeTextPaste: (text) {
-                      return true;
-                    },
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 38,
-                    child: ElevatedButton(
-                        style: appButtonStyle(),
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const ResetPasswordScreen()));
-                        },
-                        child: const Text("Verify")),
-                  ),
-                  const SizedBox(
-                    height: 48,
-                  ),
-                  Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          "Have an account!",
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.black54),
+                    const Text(
+                      "PIN Verification",
+                      style:
+                          TextStyle(fontSize: 32, fontWeight: FontWeight.w600),
+                    ),
+                    const SizedBox(
+                      height: 18,
+                    ),
+                    const Text(
+                      "A 6 digit verification pin will send to your\nemail address",
+                      style: TextStyle(fontSize: 16, color: Colors.grey),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    PinCodeTextField(
+                        controller: _otpVTEController,
+                        appContext: context,
+                        length: 6,
+                        obscureText: false,
+                        animationType: AnimationType.fade,
+                        pinTheme: PinTheme(
+                          shape: PinCodeFieldShape.box,
+                          borderRadius: BorderRadius.circular(5),
+                          fieldHeight: 50,
+                          fieldWidth: 40,
+                          activeFillColor: Colors.white,
+                          selectedFillColor: Colors.white,
+                          inactiveFillColor: Colors.white,
+                          activeColor: Colors.greenAccent,
                         ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pushAndRemoveUntil(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const SignInScreen()),
-                                (route) => false);
-                          },
-                          child: const Text(
-                            "Sign in",
+                        animationDuration: const Duration(milliseconds: 300),
+                        enableActiveFill: true,
+                        onCompleted: (v) {},
+                        onChanged: (value) {},
+                        beforeTextPaste: (text) {
+                          return true;
+                        },
+                        validator: (String? value) {
+                          if (value?.trim().isEmpty ?? true) {
+                            return 'PIN field in empty!';
+                          } else if (value!.length < 6) {
+                            return 'The PIN must have 6 digits.';
+                          }
+                          return null;
+                        }),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 38,
+                      child: Visibility(
+                        visible: _pinVerificationInProgress == false,
+                        replacement: const Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                        child: ElevatedButton(
+                            style: appButtonStyle(),
+                            onPressed: () {
+                              pinVerification();
+                            },
+                            child: const Text("Verify")),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 48,
+                    ),
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Have an account!",
                             style: TextStyle(
                                 fontSize: 16,
                                 fontWeight: FontWeight.w600,
-                                color: Colors.greenAccent),
+                                color: Colors.black54),
                           ),
-                        )
-                      ],
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushAndRemoveUntil(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SignInScreen()),
+                                  (route) => false);
+                            },
+                            child: const Text(
+                              "Sign in",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.greenAccent),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  Future<void> pinVerification() async {
+    if (_formKey.currentState!.validate()) {
+      String? emailAddress = await readUserData("EmailVerification");
+      _pinVerificationInProgress = true;
+      if (mounted) {
+        setState(() {});
+      }
+      final response = await NetworkCaller()
+          .getRequest(Urls.verifyOTP(emailAddress!, _otpVTEController.text));
+
+      if (response.isSuccess) {
+        await writePINVerification(_otpVTEController.text);
+        _pinVerificationInProgress = false;
+        if (mounted) {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const ResetPasswordScreen()));
+          setState(() {});
+        }
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _otpVTEController.dispose();
+    super.dispose();
   }
 }
